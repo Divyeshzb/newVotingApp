9a93e3de66c535437e429d61b9e119a6f4436a52
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-04-05 11:49:29 +0530

    Roost Commit

--- RoostTest/election-commission/election-commission.spec.js
+++ RoostTest/election-commission/election-commission.spec.js
@@ -1,9 +1,26 @@
-WebDriver driver = new ChromeDriver();
-String url = System.getenv("ROOST_SVC_URL");
+const webdriver = require('selenium-webdriver');
+const By = webdriver.By;
+const until = webdriver.until;
+const url = process.env.ROOST_SVC_URL;
+const server = process.env.SELENIUM_SERVER;
+
+const driver = new webdriver.Builder()
+  .usingServer(server)
+  .withCapabilities(webdriver.Capabilities.chrome())
+  .build();
+
 driver.get(url);
-Assert.assertTrue(driver.getPageSource().contains("Election Commission Admin Portal"));
-driver.findElement(By.id("add-candidate-btn")).click();
-Assert.assertTrue(driver.getPageSource().contains("Add your candidates for election of K8s distribution here"));
-Assert.assertTrue(driver.getPageSource().contains("Candidate's Name"));
-Assert.assertTrue(driver.getPageSource().contains("Candidate's Picture"));
+
+driver.wait(until.titleContains('Election Commission Admin Portal'), 5000);
+
+driver.findElement(By.xpath('//button[text()="Add Candidate"]')).click();
+
+driver.wait(until.titleContains('Add Candidate'), 5000);
+
+driver.wait(until.elementTextContains(driver.findElement(By.tagName('body')), 'Add your candidates for election of K8s distribution here'), 5000);
+
+driver.wait(until.elementTextContains(driver.findElement(By.tagName('body')), "Candidate's Name"), 5000);
+
+driver.wait(until.elementTextContains(driver.findElement(By.tagName('body')), "Candidate's Picture"), 5000);
+
 driver.quit();
