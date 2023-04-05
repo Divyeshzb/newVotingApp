0eb69a2813cfa517ce57fa40a67dd92b89da7830
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-04-05 16:31:02 +0530

    Roost Commit

--- RoostTest/voter/voter.spec.js
+++ RoostTest/voter/voter.spec.js
@@ -1,31 +1,38 @@
-const webdriver = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 
-const roostSvcUrl = process.env.ROOST_SVC_URL;
-const seleniumServer = process.env.SELENIUM_SERVER;
-
-const driver = new webdriver.Builder()
+async function test() {
+  const svcUrl = process.env.ROOST_SVC_URL;
+  const seleniumServer = process.env.SELENIUM_SERVER;
+  
+  const driver = await new Builder()
     .usingServer(seleniumServer)
-    .withCapabilities(webdriver.Capabilities.chrome())
+    .forBrowser('chrome')
     .build();
+  
+  await driver.get(svcUrl);
+  
+  const cardContents = await driver.findElements(By.className('cardContent'));
+  
+  for (let i = 0; i < cardContents.length; i++) {
+    await cardContents[i].click();
+    const postRequests = await driver.executeScript(`
+      return window.performance.getEntries()
+        .filter(entry => entry.initiatorType === 'xmlhttprequest' && entry.name.includes('/ballot'))
+        .map(entry => entry.name);
+    `);
+    assert(postRequests.length > 0, 'No post requests with ballot endpoint found');
+  }
+  
+  const showResultsButton = await driver.findElement(By.id('showResultsButton'));
+  const isButtonVisible = await showResultsButton.isDisplayed();
+  assert(isButtonVisible, 'Show Results button is not visible');
+  
+  await showResultsButton.click();
+  const currentUrl = await driver.getCurrentUrl();
+  assert(currentUrl.includes('/voter/result'), 'Current page is not a result page');
+  
+  await driver.quit();
+}
 
-driver.get(roostSvcUrl);
-
-driver.findElements(webdriver.By.className('cardContent'))
-    .then(elements => {
-        elements.forEach(element => {
-            element.click();
-            driver.wait(webdriver.until.elementLocated(webdriver.By.css('post-api-call[endpoint="ballot"]')), 5000);
-        });
-    });
-
-driver.findElements(webdriver.By.css('button.show-results'))
-    .then(elements => {
-        if (elements.length > 0) {
-            elements[0].click();
-            driver.wait(webdriver.until.urlContains('/voter/result'), 5000);
-            assert.ok(driver.getCurrentUrl().includes('/voter/result'));
-        }
-    });
-
-driver.quit();
+test();
