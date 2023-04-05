645d904f956831ebad78efa319fc8f6fd54996e3
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-04-05 12:13:28 +0530

    Roost Commit

--- RoostTest/voter/voter.spec.js
+++ RoostTest/voter/voter.spec.js
@@ -1,29 +1,37 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const assert = require('assert');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const proxy = require('selenium-webdriver/proxy');
+
+const ROOST_SVC_URL = `${ROOST_SVC_URL}`;
+const SELENIUM_SERVER = `${SELENIUM_SERVER}`;
+const BALLOT_ENDPOINT = '/api/ballot';
+const RESULTS_ENDPOINT = '/voter/result';
 
 (async function example() {
-  let driver = await new Builder().forBrowser("chrome")
-        .usingServer(`${SELENIUM_SERVER}`)
-        .build();
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .usingServer(SELENIUM_SERVER)
+    .setProxy(proxy.manual({http: 'http://my.proxy.com:8080'}))
+    .build();
   try {
-    await driver.get(`${ROOST_SVC_URL}`);
-    
-    const cardContents = await driver.findElements(By.className('cardContent'));
-    for (const cardContent of cardContents) {
-      await cardContent.click();
-      await driver.executeScript('window.stop();'); // stop page from loading to intercept post api call
-      const networkLogs = await driver.manage().logs().get('performance');
-      const ballotEndpointCalls = networkLogs.filter(log => log.message.includes('/ballot'));
-      assert(ballotEndpointCalls.length > 0, 'No post api call with ballot endpoint found.');
+    await driver.get(ROOST_SVC_URL);
+    let cardContents = await driver.findElements(By.className('cardContent'));
+    for (let i = 0; i < cardContents.length; i++) {
+      await cardContents[i].click();
+      await driver.wait(until.elementLocated(By.css('post')), 10000);
+      let postRequest = await driver.executeScript('return window.performance.getEntries()[0].name;');
+      if (postRequest.includes(BALLOT_ENDPOINT)) {
+        console.log('API call intercepted successfully');
+      }
     }
-    
-    const showResultsButton = await driver.findElement(By.xpath('//button[contains(text(), "Show Results")]'));
-    assert(await showResultsButton.isDisplayed(), 'Show Results button is not visible.');
-    
-    await showResultsButton.click();
-    const currentUrl = await driver.getCurrentUrl();
-    assert(currentUrl.includes('/voter/result'), 'Redirect url does not contain /voter/result.');
+    let showResultsButton = await driver.findElement(By.id('showResultsButton'));
+    if (await showResultsButton.isDisplayed()) {
+      await showResultsButton.click();
+      let currentUrl = await driver.getCurrentUrl();
+      if (currentUrl.includes(RESULTS_ENDPOINT)) {
+        console.log('Redirected to results page successfully');
+      }
+    }
   } finally {
     await driver.quit();
   }
-})()
+})();
