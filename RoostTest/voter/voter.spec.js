32de9bad3dfedd478113804e52fd0d4f5933fe69
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-04-05 15:41:47 +0530

    Roost Commit

--- RoostTest/voter/voter.spec.js
+++ RoostTest/voter/voter.spec.js
@@ -1,23 +1,47 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const fetch = require('node-fetch');
 
 (async function example() {
-  let driver = await new Builder().usingServer(`${SELENIUM_SERVER}`).forBrowser('chrome').build();
+  let driver = await new Builder()
+    .usingServer(process.env.SELENIUM_SERVER)
+    .forBrowser('chrome')
+    .build();
   try {
-    await driver.get(`${ROOST_SVC_URL}`);
-    let cardContents = await driver.findElements(By.className('cardContent'));
-    for (let i = 0; i < cardContents.length; i++) {
-      await cardContents[i].click();
+    await driver.get(process.env.ROOST_SVC_URL);
+    const cardContents = await driver.findElements(By.className('cardContent'));
+    for (const cardContent of cardContents) {
+      await cardContent.click();
       await driver.wait(until.urlContains('/ballot'));
-      let logs = await driver.manage().logs().get('performance');
-      let ballotApiCalls = logs.filter(log => log.message.includes('ballot'));
-      console.log(ballotApiCalls);
+      const interceptedRequest = await driver.executeScript(`
+        const fetch = window.fetch;
+        window.fetch = function (url, options) {
+          if (url.includes('/ballot') && options.method === 'POST') {
+            return fetch(url, options).then((response) => {
+              window.interceptedRequest = {
+                url,
+                options,
+                response: {
+                  status: response.status,
+                  body: response.json(),
+                },
+              };
+              return response;
+            });
+          }
+          return fetch(url, options);
+        };
+      `);
+      const interceptedRequestBody = await driver.executeScript('return window.interceptedRequest.options.body;');
+      const response = await fetch(interceptedRequest.url, interceptedRequest.options);
+      const responseBody = await response.json();
+      console.log(interceptedRequestBody, responseBody);
     }
-    let showResultsButton = await driver.findElement(By.id('showResultsButton'));
-    let isDisplayed = await showResultsButton.isDisplayed();
-    if (isDisplayed) {
+    const showResultsButton = await driver.findElement(By.id('show-results-button'));
+    const isShowResultsButtonVisible = await showResultsButton.isDisplayed();
+    if (isShowResultsButtonVisible) {
       await showResultsButton.click();
       await driver.wait(until.urlContains('/voter/result'));
-      let currentUrl = await driver.getCurrentUrl();
+      const currentUrl = await driver.getCurrentUrl();
       console.log(currentUrl);
     }
   } finally {
